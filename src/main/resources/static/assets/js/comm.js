5b2b52546ea27ae1d87803e6747e6c7365a050ef
fredma1987 <356803326@qq.com>
2019-01-31 15:37:26 +0800

    称重

--- src/main/resources/static/assets/js/comm.js
+++ src/main/resources/static/assets/js/comm.js
@@ -18,3 +18,84 @@ function turnArrayToJson(data) {
     }
     return json;
 }
+function getDataJson(url, param) {
+    var json;
+    if (param == undefined)
+        param = {};
+        $.ajax({
+            type: "POST",// 可选
+            async: false,
+            url: url,
+            data: param,
+            dataType: "json",
+            success: function (result) {
+                json= result;
+            },
+            error: function (msg) {
+                alert("请求超时");
+            }
+        });
+    return json;
+}
+function comm_initInspresult(){
+    var data = [{value: 1, text: "待检"}, {value: 2, text: "接收"}, {value: 3, text: "不接收"}];
+    $("#inspresult").bootstrapSelect({
+        data: data,
+        valueField: 'value',
+        textField: 'text',
+        defaultValue: 1
+    });
+}
+function comm_initGrain() {
+    $("#grainid").bootstrapSelect({
+        url: '/graindepot-base/selector/grainList',
+        type: 'GET',
+        valueField: 'grainid',
+        textField: 'grainname'
+    });
+}
+function comm_initGrainattr() {
+    $("#grainattrid").bootstrapSelect({
+        url:  '/graindepot-base/selector/grainattrList',
+        type: 'GET',
+        valueField: 'grainattrid',
+        textField: 'grainattrname'
+    });
+}
+function comm_initGrade(){
+    var data = [{value: 1, text: "一等"}, {value: 2, text: "二等"}, {value: 3, text: "三等"}
+        , {value: 4, text: "四等"}, {value: 5, text: "五等"}, {value: 6, text: "等外"}];
+    $("#grade").bootstrapSelect({
+        data: data,
+        valueField: 'value',
+        textField: 'text',
+        defaultValue: 3
+    });
+}
+function comm_initSettlemothod() {
+    var data = [{value: 1, text: "现金结算"}, {value: 2, text: "银行卡结算"}, {value: 3, text: "微信结算"}, {value: 4, text: "支付宝结算"}];
+    $("#settlemothod").bootstrapSelect({
+        data: data,
+        valueField: 'value',
+        textField: 'text',
+        defaultValue: 1
+    });
+}
+function comm_initTrucktype() {
+    var data = [{value: 1, text: "车辆"}, {value: 2, text: "船舶"}, {value: 3, text: "火车"}, {value: 99, text: "其他"}];
+    $("#trucktype").bootstrapSelect({
+        data: data,
+        valueField: 'value',
+        textField: 'text',
+        defaultValue: 1
+    });
+}
+function comm_initStorage(param) {
+    $("#storageid").bootstrapSelect({
+        url: '/graindepot-base/selector/storageList',
+        type: 'GET',
+        valueField: 'storageid',
+        textField: 'storagename',
+        param:param
+    });
+}
