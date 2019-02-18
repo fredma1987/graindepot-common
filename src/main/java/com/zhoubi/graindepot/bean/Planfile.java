bc53bbb97cb50a618946ea3ed5af95bf57a37a09
fredma1987 <356803326@qq.com>
2019-02-18 23:08:56 +0800

    调整大小写

--- src/main/java/com/zhoubi/graindepot/bean/Planfile.java
+++ src/main/java/com/zhoubi/graindepot/bean/Planfile.java
@@ -3,29 +3,44 @@ import java.io.Serializable;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 public class Planfile implements Serializable {
-	private int planfileid;//计划主键
-	private int groupid;//集团ID
-	private int companyid;//企业ID
-	private int graindepotid;//粮库ID
-	private int planTypeID;//计划类型主键(1：轮换计划，2：轮出计划，3：轮入计划)
-	private String planYear;//计划年度
-	private String planNo;//计划文号
+	private Integer planfileid;//计划主键
+	private Integer groupid;//集团ID
+	private Integer companyid;//企业ID
+	private Integer graindepotid;//粮库ID
+	private Integer plantypeid;//计划类型主键(1：轮换计划，2：轮出计划，3：轮入计划)
+	private Integer planyear;//计划年度
+	private String planno;//计划文号
 	private String dispatchunit;//下达单位
 	private Date dispatchdate;//下达日期
-	private Date planBeginDate;//计划开始日期
-	private Date planEndDate;//计划结束日期
-	private String fileNameList;//附加文件清单，分号隔开
-	private short delFlag;//逻辑删除标志（1：删除）
+	private Date planbegindate;//计划开始日期
+	private Date planenddate;//计划结束日期
+	private String filenamelist;//附加文件清单，分号隔开
+	private Short delflag;//逻辑删除标志（1：删除）
 	private String remark;//备注
-	private int createuserid;//创建人
+	private Integer createuserid;//创建人
 	private Date createtime;//创建时间
-	private String planTypeIDStr;//计划类型
-	private String dispatchdateStr;
-	private String planBeginDateStr;
-	private String planEndDateStr;
+
+	private String plantypeidstr;//计划类型
 	private String companyname;//企业名称
 	private String graindepotname;//库点名称
 	private String createusername;//创建人姓名
+
+	public String getPlantypeidstr() {
+		if (plantypeid==1) {
+			return "轮换计划";
+		}
+		if (plantypeid==2) {
+			return "轮出计划";
+		}
+		if (plantypeid==3) {
+			return "轮入计划";
+		}
+		return plantypeidstr;
+	}
+
+	public void setPlantypeidstr(String planttypeidstr) {
+		this.plantypeidstr = planttypeidstr;
+	}
 
 	public String getCompanyname() {
 		return companyname;
@@ -51,76 +66,47 @@ public class Planfile implements Serializable {
 		this.createusername = createusername;
 	}
 
-	public void setPlanBeginDateStr(String planBeginDateStr) {
-		this.planBeginDateStr = planBeginDateStr;
-	}
-
-	public void setPlanEndDateStr(String planEndDateStr) {
-		this.planEndDateStr = planEndDateStr;
-	}
-
-	public void setDispatchdateStr(String dispatchdateStr) {
-		this.dispatchdateStr = dispatchdateStr;
-	}
-
-	public String getPlanTypeIDStr() {
-		if (planTypeID==1) {
-			return "轮换计划";
-		}
-		if (planTypeID==2) {
-			return "轮出计划";
-		}
-		if (planTypeID==3) {
-			return "轮入计划";
-		}
-		return planTypeIDStr;
-	}
-
-	public void setPlanTypeIDStr(String plantTypeIDStr) {
-		this.planTypeIDStr = plantTypeIDStr;
-	}
-
-	public int getPlanfileid(){
+	public Integer getPlanfileid(){
 		return planfileid;
 	}
-	public void setPlanfileid(int planfileid){
+	public void setPlanfileid(Integer planfileid){
 		this.planfileid=planfileid;
 	}
-	public int getGroupid(){
+	public Integer getGroupid(){
 		return groupid;
 	}
-	public void setGroupid(int groupid){
+	public void setGroupid(Integer groupid){
 		this.groupid=groupid;
 	}
-	public int getCompanyid(){
+	public Integer getCompanyid(){
 		return companyid;
 	}
-	public void setCompanyid(int companyid){
+	public void setCompanyid(Integer companyid){
 		this.companyid=companyid;
 	}
-	public int getGraindepotid(){
+	public Integer getGraindepotid(){
 		return graindepotid;
 	}
-	public void setGraindepotid(int graindepotid){
+	public void setGraindepotid(Integer graindepotid){
 		this.graindepotid=graindepotid;
 	}
-	public int getPlanTypeID(){
-		return planTypeID;
+	public Integer getPlantypeid(){
+		return plantypeid;
 	}
-	public void setPlanTypeID(int planTypeID){
-		this.planTypeID=planTypeID;
+	public void setPlantypeid(Integer plantypeid){
+		this.plantypeid=plantypeid;
 	}
-	public String getPlanYear(){
-		return planYear;
+	public Integer getPlanyear(){
+		return planyear;
 	}
-	public void setPlanYear(String planYear){
-		this.planYear=planYear;
+	public void setPlanyear(Integer planyear){
+		this.planyear=planyear;
 	}
-	public String getPlanNo(){
-		return planNo;
+	public String getPlanno(){
+		return planno;
 	}
-	public void setPlanNo(String planNo){
-		this.planNo=planNo;
+	public void setPlanno(String planno){
+		this.planno=planno;
 	}
 	public String getDispatchunit(){
 		return dispatchunit;
@@ -128,9 +114,13 @@ public class Planfile implements Serializable {
 	public void setDispatchunit(String dispatchunit){
 		this.dispatchunit=dispatchunit;
 	}
-	public String getDispatchdateStr(){
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		return dispatchdate == null ? dispatchdateStr : simpleDateFormat.format(dispatchdate);
+	private String dispatchdatestr;
+	public void setDispatchdatestr(String dispatchdatestr){
+		this.dispatchdatestr=dispatchdatestr;
+	}
+	public String getDispatchdatestr(){
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return dispatchdate == null ? dispatchdatestr : simpleDateFormat.format(dispatchdate);
 	}
 	public Date getDispatchdate(){
 		return dispatchdate;
@@ -138,37 +128,45 @@ public class Planfile implements Serializable {
 	public void setDispatchdate(Date dispatchdate){
 		this.dispatchdate=dispatchdate;
 	}
-	public String getPlanBeginDateStr(){
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		return planBeginDate == null ? planBeginDateStr : simpleDateFormat.format(planBeginDate);
+	private String planbegindatestr;
+	public void setPlanbegindatestr(String planbegindatestr){
+		this.planbegindatestr=planbegindatestr;
 	}
-	public Date getPlanBeginDate(){
-		return planBeginDate;
+	public String getPlanbegindatestr(){
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return planbegindate == null ? planbegindatestr : simpleDateFormat.format(planbegindate);
 	}
-	public void setPlanBeginDate(Date planBeginDate){
-		this.planBeginDate=planBeginDate;
+	public Date getPlanbegindate(){
+		return planbegindate;
 	}
-	public String getPlanEndDateStr(){
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		return planEndDate == null ? planEndDateStr : simpleDateFormat.format(planEndDate);
+	public void setPlanbegindate(Date planbegindate){
+		this.planbegindate=planbegindate;
 	}
-	public Date getPlanEndDate(){
-		return planEndDate;
+	private String planenddatestr;
+	public void setPlanenddatestr(String planenddatestr){
+		this.planenddatestr=planenddatestr;
 	}
-	public void setPlanEndDate(Date planEndDate){
-		this.planEndDate=planEndDate;
+	public String getPlanenddatestr(){
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return planenddate == null ? planenddatestr : simpleDateFormat.format(planenddate);
 	}
-	public String getFileNameList(){
-		return fileNameList;
+	public Date getPlanenddate(){
+		return planenddate;
 	}
-	public void setFileNameList(String fileNameList){
-		this.fileNameList=fileNameList;
+	public void setPlanenddate(Date planenddate){
+		this.planenddate=planenddate;
 	}
-	public short getDelFlag(){
-		return delFlag;
+	public String getFilenamelist(){
+		return filenamelist;
 	}
-	public void setDelFlag(short delFlag){
-		this.delFlag=delFlag;
+	public void setFilenamelist(String filenamelist){
+		this.filenamelist=filenamelist;
+	}
+	public Short getDelflag(){
+		return delflag;
+	}
+	public void setDelflag(Short delflag){
+		this.delflag=delflag;
 	}
 	public String getRemark(){
 		return remark;
@@ -176,15 +174,19 @@ public class Planfile implements Serializable {
 	public void setRemark(String remark){
 		this.remark=remark;
 	}
-	public int getCreateuserid(){
+	public Integer getCreateuserid(){
 		return createuserid;
 	}
-	public void setCreateuserid(int createuserid){
+	public void setCreateuserid(Integer createuserid){
 		this.createuserid=createuserid;
 	}
-	public String getCreatetimeStr(){
+	private String createtimestr;
+	public void setCreatetimestr(String createtimestr){
+		this.createtimestr=createtimestr;
+	}
+	public String getCreatetimestr(){
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		return createtime == null ? "" : simpleDateFormat.format(createtime);
+		return createtime == null ? createtimestr : simpleDateFormat.format(createtime);
 	}
 	public Date getCreatetime(){
 		return createtime;
